0b8776919f03afc92d0fb1fa4a8a11d587161db7
JoniPahikainen <joni.pahikainen01@gmail.com>
2024-03-19 16:19:36 +0200

    fix useEffect error log

--- frontend/App.js
+++ frontend/App.js
@@ -7,12 +7,15 @@ export default function App() {
   const [data, setData] = useState("");
 
   useEffect(() => {
-    axios.get("/test").then((res) => {
-      setData(res.data);
-      console.log(res.data).catch((err) => {
+    axios
+      .get("/test")
+      .then((res) => {
+        setData(res.data);
+        console.log(res.data);
+      })
+      .catch((err) => {
         console.log(err);
       });
-    });
   }, []);
 
   return (
